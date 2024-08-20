15ef1c90bd750fc566791ea9e63eb8b4298a219e
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-20 01:14:37 -0300

    format base64

--- base64.go
+++ base64.go
@@ -3,9 +3,9 @@ package main
 import "encoding/base64"
 
 func EncodeBase64(input string) string {
-  src := []byte(input)
-  encoded := make([]byte, base64.StdEncoding.EncodedLen(len(src)))
-  base64.StdEncoding.Encode(encoded, src)
+	src := []byte(input)
+	encoded := make([]byte, base64.StdEncoding.EncodedLen(len(src)))
+	base64.StdEncoding.Encode(encoded, src)
 
-  return string(encoded)
+	return string(encoded)
 }
