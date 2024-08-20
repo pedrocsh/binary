5906703332cac0e0f64da088feec3f477ba5f5c4
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-20 00:56:49 -0300

    format hex

--- hex.go
+++ hex.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"encoding/hex"
-	"fmt"
 )
 
 func DecodeHex(input string) string {
 	src := []byte(input)
 	dst := make([]byte, len(src))
 	decoded, _ := hex.Decode(dst, src)
-	fmt.Println(dst)
 
 	return string(dst[:decoded])
 }
